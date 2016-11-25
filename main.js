c5bd3555391afc51b0748f90907ade8009d665da
dannyboy <a@b.cde>
2016-11-25 16:46:54 +0100

    Add builder

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var config = require('config');
 var tools = require('tools');
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
 var creepProto = require('creep');
 
 var energySource = creepProto.findNearestSource();
@@ -11,15 +12,21 @@ module.exports.loop = function () {
     tools.clearMemory();
     var harvesters = creepProto.findAllByRole('harvester');
     var upgraders = creepProto.findAllByRole('upgrader');
+    var builders = creepProto.findAllByRole('builder');
 
-    if (harvesters.length < 3) {
+    if (harvesters.length < 1) {
         var newName = Game.spawns[config.mainSpawn].createCreep([WORK, CARRY, MOVE], undefined, {role: 'harvester'});
         console.log('Spawning new harvester: ' + newName);
     }
 
-    if (upgraders.length < 2) {
+    if (upgraders.length < 3) {
         var newName = Game.spawns[config.mainSpawn].createCreep([WORK, CARRY, MOVE], undefined, {role: 'upgrader'});
         console.log('Spawning new upgrader: ' + newName);
+    }
+
+    if (builders.length < 1) {
+        var newCreep = Game.spawns[config.mainSpawn].createCreep([WORK, CARRY, MOVE], undefined, {role: 'builder'});
+        console.log('Spawning new builder: ' + newCreep);
     }
 
     for (var name in Game.creeps) {
@@ -29,6 +36,9 @@ module.exports.loop = function () {
         }
         if (creep.memory.role == 'upgrader') {
             roleUpgrader.run(creep);
+        }
+        if (creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
         }
     }
 };
