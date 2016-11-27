7dec839f14e1e9e7953124099a73d2273ac3dbad
dannyboy <a@b.cde>
2016-11-27 21:30:34 +0100

    Add source distribution

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {
     mainSpawn                  : 'Mainframe',
     maxHarvesters              : 4,
     maxBuilder                 : 3,
-    maxUpgrader                : 2,
+    maxUpgrader                : 3,
     emergencyHarvesterBodyParts: [WORK, CARRY, CARRY, MOVE, MOVE]
 };
 
